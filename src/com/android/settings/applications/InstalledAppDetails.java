f2ab09c751cbdd023e9fd23ead1368728e6757ba
fusionjack <dogfight60-fusionjack@yahoo.de>
2014-06-22 12:42:42 +0200

    Revert "Enable moving applications to an external volume"
    This reverts commit e941100bb2881e6e37365892c09e7eaa742ea0ef.

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -289,7 +289,7 @@ public class InstalledAppDetails extends Fragment
     }
 
     private void initMoveButton() {
-        if (!Environment.isExternalAppsAvailableAndMounted()) {
+        if (Environment.isExternalStorageEmulated()) {
             mMoveAppButton.setVisibility(View.INVISIBLE);
             return;
         }
@@ -472,7 +472,7 @@ public class InstalledAppDetails extends Fragment
         mExternalCodeSize = (TextView)view.findViewById(R.id.external_code_size_text);
         mExternalDataSize = (TextView)view.findViewById(R.id.external_data_size_text);
 
-        if (!Environment.isExternalAppsAvailableAndMounted()) {
+        if (Environment.isExternalStorageEmulated()) {
             ((View)mExternalCodeSize.getParent()).setVisibility(View.GONE);
             ((View)mExternalDataSize.getParent()).setVisibility(View.GONE);
         }
@@ -989,7 +989,7 @@ public class InstalledAppDetails extends Fragment
             mHaveSizes = true;
             long codeSize = mAppEntry.codeSize;
             long dataSize = mAppEntry.dataSize;
-            if (!Environment.isExternalAppsAvailableAndMounted()) {
+            if (Environment.isExternalStorageEmulated()) {
                 codeSize += mAppEntry.externalCodeSize;
                 dataSize +=  mAppEntry.externalDataSize;
             } else {
